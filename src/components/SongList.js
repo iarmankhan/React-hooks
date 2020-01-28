40371148fc6b17386b4c96bb41c20ff46d752413
iarmankhan <armankhan9244@gmail.com>
2020-01-28 12:40:45 +0530

    useState with forms

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
+import uuid from 'uuid';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
-    const [songs, setSongs] = useState([
+    const [songs, setSongs] = useState([    
         {title: 'Almost home', id: 1},
         {title: 'Memory Gospel', id: 2},
         {title: 'This wild darkness', id: 3},
     ]);
-    const addSong = () => {
-        setSongs([...songs, {title: 'NewSong', key: 4}])
+    const addSong = title => {
+        setSongs([...songs, {title, key: uuid()}])
     };
     return (
         <div className="song-list">
             <ul>
                 {songs.map(song => <li key={song.id}>{song.title}</li>)}
             </ul>
-            <button onClick={addSong}>Add Song</button>
+            <NewSongForm addSong={addSong} />
         </div>
     );
 }
